e028551a07615753b7b993205c65244200629994
Jwyydd <johnathanwilliams55555@gmail.com>
2025-11-16 17:28:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+# Step 5: First Python Program
+
+# Print "Hello World!"
+print("Hello World!")
+
+# Ask the user for their name
+username = input("What is your name? ")
+
+# Print back the message
+print(username + " is cool!")
